bfd2595e15ab2651da71550ea9110a2d0c854929
Sangimed <med.ibrihen@gmail.com>
2024-12-21 16:19:40 +0100

    mail module skull code 💀

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailModule } from './modules/mail/mail.module';
 
 @Module({
-  imports: [],
+  imports: [MailModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
