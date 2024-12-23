959d368cc159d9c103afc13a3c8fb3c2df8cc9c5
Mohamed IBRIHEN <med.ibrihen@gmail.com>
2024-12-24 00:01:31 +0100

    skull code is ooon'ish

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,7 +1,34 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: 'localhost',
+          port: 1025,
+          secure: false,
+          tls: {
+            rejectUnauthorized: false,
+          },
+        },
+        defaults: {
+          from: '"Sangimed" <sangimed@dumbmail.me>',
+        },
+        template: {
+          dir: __dirname + '/../../templates',
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
   providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}
--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { ISendMailOptions, MailerService } from '@nestjs-modules/mailer';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  private readonly logger = new Logger(MailService.name);
+
+  constructor(private readonly mailerService: MailerService) {}
+
+  async sendEmail(params: {
+    subject: string;
+    template: string;
+    context: ISendMailOptions['context'];
+  }) {
+    try {
+      const emailsList: string[] = process.env.SMTP_TO?.split(',');
+
+      if (!emailsList) {
+        throw new Error(
+          `No recipients found in SMTP_TO env var, please check your .env file`,
+        );
+      }
+
+      const sendMailParams = {
+        to: emailsList,
+        from: process.env.SMTP_FROM || 'no-reply@nestjs.com',
+        subject: params.subject,
+        template: params.template,
+        context: params.context,
+      };
+      const response = await this.mailerService.sendMail(sendMailParams);
+      this.logger.log(
+        `Envoi du mail OK avec les paramètres suivants : ${JSON.stringify(
+          sendMailParams,
+        )}`,
+        response,
+      );
+    } catch (error) {
+      this.logger.error(
+        `Erreur lors de l'envoi du mail avec les paramètres suivants : ${JSON.stringify(
+          params,
+        )}`,
+        error,
+      );
+    }
+  }
+}
